0b2f0aff0ed83e32adbd51346045770466ed59d3
anu <85746283+anuradha20@users.noreply.github.com>
2024-06-08 23:21:46 +0530

    Add files via upload

---
+++ HTML PROJECT/Amazon clone/Cascading style sheet documents/home.js
@@ -0,0 +1,16 @@
+import React from 'React'
+
+function Home(){
+    return(
+        <div className='container'>
+           <div className='weather'>
+            <div className='search'>
+<input type="text" placeholder='Enter City Name'/>
+   <button><img src="/images</button>
+            </div>
+           </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ HTML PROJECT/index.js
@@ -0,0 +1,14 @@
+  console.log("Hello World");
+var n1 =56 ;
+var n2 =65;
+console.log(n1+n2);
+var str1 = "this is a string";
+var str2 = "string";
+
+var marks={
+    ravi: 32,
+    kumar: 35
+}
+
+var arr=[1,2,3,4,5];
+console.log(arr);  
